6702bc909a2be6ed85bff3093c7bc6576fc444d9
Justin Park <justindypark@yahoo.com>
2018-11-03 04:45:16 -0700

    Multiplayer body spawn

--- CalHacks2018/Assets/NetworkPlayerBehaviour.cs
+++ CalHacks2018/Assets/NetworkPlayerBehaviour.cs
@@ -5,17 +5,71 @@ using UnityEngine.Networking;
 
 public class NetworkPlayerBehaviour : NetworkBehaviour {
     public GameObject carPrefab;
+
+    public GameObject headsetPrefab;
+    public GameObject headset;
+
+    public GameObject handPrefab;
+    public GameObject leftHand;
+    public GameObject rightHand;
 	// Use this for initialization
 	void Start () {
-        if(!isServer) {
-            return;
+        if(isServer) {
+            if (!GameObject.Find("Car(Clone)"))
+            {
+                GameObject car = (GameObject)Instantiate(carPrefab, Vector3.zero, Quaternion.identity);
+                NetworkServer.Spawn(car);
+            }
         }
-        GameObject car = (GameObject)Instantiate(carPrefab, Vector3.zero, Quaternion.identity);
-        NetworkServer.Spawn(car);
+        if (isLocalPlayer)
+        {
+            transform.parent = GameObject.Find("Car(Clone)").transform;
+            transform.localPosition = Vector3.zero;
+            CmdSpawnBody();
+
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(isLocalPlayer) {
+            if(Input.GetKey("d")) {
+                Vector3 pos = transform.position;
+                pos.x += 0.05f;
+                transform.position = pos;
+            }
+            if (Input.GetKey("a"))
+            {
+                Vector3 pos = transform.position;
+                pos.x -= 0.05f;
+                transform.position = pos;
+            }
+
+            headset.transform.position = transform.position;
+            leftHand.transform.position = transform.position;
+            rightHand.transform.position = transform.position;
+        }
 	}
+    [Command]
+    public void CmdSpawnBody()
+    {
+        headset = (GameObject)Instantiate(headsetPrefab, transform.position, transform.rotation);
+        leftHand = (GameObject)Instantiate(handPrefab, transform.position, transform.rotation);
+        rightHand = (GameObject)Instantiate(handPrefab, transform.position, transform.rotation);
+
+        NetworkServer.SpawnWithClientAuthority(headset, gameObject);
+        NetworkServer.SpawnWithClientAuthority(leftHand, gameObject);
+        NetworkServer.SpawnWithClientAuthority(rightHand, gameObject);
+        RpcRegisterBody(headset, leftHand, rightHand);
+    }
+
+    [ClientRpc]
+    public void RpcRegisterBody(GameObject head, GameObject lHand, GameObject rHand)
+    {
+        if(isLocalPlayer) {
+            headset = head;
+            leftHand = lHand;
+            rightHand = rHand;
+        }
+    }
 }
