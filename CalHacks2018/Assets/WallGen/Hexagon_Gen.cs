db6ec3dafcfb5c77bbcb90392f1cf85c6279410e
Justin Park <justindypark@yahoo.com>
2018-11-04 03:12:41 -0800

    Constant

--- CalHacks2018/Assets/WallGen/Hexagon_Gen.cs
+++ CalHacks2018/Assets/WallGen/Hexagon_Gen.cs
@@ -6,9 +6,10 @@ public class Hexagon_Gen : MonoBehaviour
 {
 
     public GameObject hex_prefab;
-    public float hex_height = 5;
+    public float hex_offset = 0;
+    public float hex_height = 1f;
     float x_off = -1.5f;
-    float y_off = Mathf.Sqrt(5) * 2;
+    float y_off = Mathf.Sqrt(5) * 2f;
 
     HashSet<Vector2> phantoms;
 
@@ -16,7 +17,7 @@ public class Hexagon_Gen : MonoBehaviour
     void Start ()
     {
         phantoms = new HashSet<Vector2>();
-        phantoms.Add(new Vector2(0, 0));
+        phantoms.Add(new Vector2(0, 10));
         AddHexes(phantoms.GetEnumerator().Current);
     }
 
@@ -24,14 +25,19 @@ public class Hexagon_Gen : MonoBehaviour
     void Update ()
     {
         List<Vector2> hold = new List<Vector2>();
+        List<Vector2> remove = new List<Vector2>();
         Bounds b = GetComponent<BoxCollider>().bounds;
-        HashSet<Vector2>.Enumerator e = phantoms.GetEnumerator();
-        while (e.MoveNext()) {
-            Vector2 next = e.Current;
-            if (b.Contains(next)) {
-                hold.Add(next);
-                Debug.Log("yip");
-                StartCoroutine(SpawnHex(Random.Range(5, 20), new Vector2(next.x, next.y + Random.Range(-2, 2))));
+
+        foreach (Vector2 pos in phantoms)
+        {
+            if (b.Contains(new Vector3(pos.x, 0, pos.y)))
+            {
+                hold.Add(pos);
+                StartCoroutine(SpawnHex(pos));
+            }
+            else if (Vector3.Distance(pos, b.center) > 10)
+            {
+                remove.Add(pos);
             }
         }
 
@@ -39,21 +45,35 @@ public class Hexagon_Gen : MonoBehaviour
             AddHexes(v);
             phantoms.Remove(v);
         }
+        foreach (Vector2 v in remove)
+        {
+            phantoms.Remove(v);
+        }
+    }
+
+    void printHashSet(HashSet<Vector2> hs) {
+        foreach (Vector2 v in hs) {
+            Debug.Log(v);
+        }
     }
 
     void AddHexes (Vector2 pos) {
-        phantoms.Add(new Vector2(x_off * hex_height + pos.x, y_off * hex_height + pos.y));
+        Vector2 v = new Vector2(x_off * hex_height + pos.x, y_off * hex_height + pos.y);
+        phantoms.Add(v);
         phantoms.Add(new Vector2(x_off * hex_height + pos.x, -y_off * hex_height + pos.y));
         phantoms.Add(new Vector2(-x_off * hex_height + pos.x, y_off * hex_height + pos.y));
         phantoms.Add(new Vector2(-x_off * hex_height + pos.x, -y_off * hex_height + pos.y));
-        phantoms.Add(new Vector2(pos.x, y_off * 2 * hex_height + pos.y));
-        phantoms.Add(new Vector2(pos.x, -y_off * 2 * hex_height + pos.y));
+        phantoms.Add(new Vector2(pos.x, y_off * 2f * hex_height + pos.y));
+        phantoms.Add(new Vector2(pos.x, -y_off * 2f * hex_height + pos.y));
     }
 
-    IEnumerator SpawnHex (int time, Vector2 pos) {
-        GameObject hex = Instantiate(hex_prefab, new Vector3(pos.x, -time, pos.y), Quaternion.identity);
+    IEnumerator SpawnHex (Vector2 pos) {
+        int time = Random.Range(5, 10);
+        float offset = Random.Range(-.5f, .5f);
+        GameObject hex = Instantiate(hex_prefab, new Vector3(pos.x, offset - time + hex_offset, pos.y), Quaternion.Euler(-90, 60, 0));
+        Destroy(hex, 3);
         for (int x = 0; x < time; x++) {
-            hex.transform.position = new Vector3(pos.x, -time + x, pos.y);
+            hex.transform.position = new Vector3(pos.x, offset + x - time + hex_offset, pos.y);
             yield return null;
         }
     }
