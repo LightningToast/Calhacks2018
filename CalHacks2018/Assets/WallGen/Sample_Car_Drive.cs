473131d4c8672b28b03e433c8f2026a415bc9784
Justin Park <justindypark@yahoo.com>
2018-11-03 20:31:16 -0700

    Base wall gen

---
+++ CalHacks2018/Assets/WallGen/Sample_Car_Drive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_Car_Drive : MonoBehaviour {
+
+    int turn_counter = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        turn_counter++;
+
+        this.GetComponent<Rigidbody>().velocity = transform.forward * 10;
+
+        if (turn_counter == 200)
+        {
+            turn_counter = 0;
+            this.transform.rotation = Quaternion.Euler(0, Random.Range(-40, 40), 0);
+        }
+	}
+}
---
+++ CalHacks2018/Assets/WallGen/WallGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGen : MonoBehaviour {
+
+    public GameObject wall_prefab;
+
+    public int spawn_freq = 5;
+    int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+        spawn_freq = Mathf.Max(5, spawn_freq);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter++;
+
+        if (counter == spawn_freq)
+	}
+}
